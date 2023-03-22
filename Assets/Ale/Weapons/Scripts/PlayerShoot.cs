4d08a1c43442f3203c4a582ff03856c880464046
Alejandro <chvbale15@gmail.com>
2023-03-21 19:36:29 -0600

    Weapon no anim
    Added a fully functional weapon but has no animations

---
+++ Assets/Ale/Weapons/Scripts/PlayerShoot.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public static Action shootInput;
+    public static Action reloadInput;
+
+    [Header("Keys")]
+    [SerializeField] KeyCode reloadKey;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Will Invoke onclick, if not, it'll catch it
+            shootInput?.Invoke();
+        }
+
+        if (Input.GetKeyDown(reloadKey))
+        {
+            reloadInput?.Invoke();
+        }
+    }
+}
