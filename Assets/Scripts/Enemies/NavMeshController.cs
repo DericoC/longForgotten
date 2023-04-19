2c32dcd6d9a783c047ebb7601c277946c30b0053
monsterwhat <monster_what@hotmail.com>
2023-04-18 20:05:57 -0600

    Updates to the Spawners
    Zombies now spawn and follow the player, still has bugs.

--- Assets/Scripts/Enemies/NavMeshController.cs
+++ Assets/Scripts/Enemies/NavMeshController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Nav : MonoBehaviour
+public class NavMeshController : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float agentSpeed;
-    private Animator animator;
-    private NavMeshAgent agent;
+    [SerializeField] float agentSpeed = 1f;
+    [SerializeField] Animator animator;
+    [SerializeField] NavMeshAgent agent;
     private bool isPlayerDead = false;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.destination = player.position;
@@ -26,6 +27,60 @@ public class Nav : MonoBehaviour
             animator.SetFloat("Speed", agent.speed);
             agent.destination = player.position;
         }
+    }
+
+    private void Scream()
+    {
+        //Should be run when health is low so the enemy starts running towards the player.
+        animator.SetTrigger("Scream");
+        //wait 2 seconds before running
+        StartCoroutine(waitForScream());
+
+        StartRun();
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
+
+    private void StartRun()
+    {
+        //Starts running towards the player
+        agent.speed = agentSpeed * 2;
+        animator.SetBool("isRunning", true);
+    }
+
+    private void StopRun()
+    {
+        animator.SetBool("isRunning", false);
+        agent.speed = agentSpeed;
+    }
+
+    private void StartWalk()
+    {
+        animator.SetBool("isWalking", true);
+    }
+
+    private void StopWalk()
+    {
+        animator.SetBool("isWalking", false);
+        StartIdle();
+    }
+
+    private void StartIdle()
+    {
+        animator.SetBool("isIdle", true);
+    }
+
+    private void StopIdle()
+    {
+        animator.SetBool("isIdle", false);
+    }
+
+    IEnumerator waitForScream()
+    {
+        yield return new WaitForSeconds(2);
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+
+    public GameObject[] enemyPrefabs; // array of enemy prefabs to spawn
+    public float spawnRadius = 25f; // maximum distance from the player to spawn enemies
+    public int maxEnemiesPerSpawner = 3; // maximum number of enemies to spawn per spawner
+    public float spawnDelay = 10f; // delay between spawns of the same spawner
+    public float roundDelay = 10f; // delay between rounds
+
+    private GameObject[] spawners; // array of game objects with the "ZombieSpawner" tag
+    private GameObject player; // reference to the player object
+    private int currentRound = 1; // current round number
+
+    private void Start()
+    {
+        // find all game objects with the "ZombieSpawner" tag
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+
+        // get a reference to the player object
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // start the first round
+        StartCoroutine(SpawnEnemiesWithDelay());
+    }
+
+    private void Update()
+    {
+        // get a reference to the player object
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private IEnumerator SpawnEnemiesWithDelay()
+    {
+        // wait for the specified round delay before starting the round
+        yield return new WaitForSeconds(roundDelay);
+
+        // loop through each spawner and check if it's within the spawn radius of the player
+        foreach (GameObject spawner in spawners)
+        {
+            if (Vector3.Distance(spawner.transform.position, player.transform.position) <= spawnRadius)
+            {
+                // spawn up to maxEnemiesPerSpawner enemies at the spawner's position
+                for (int i = 0; i < maxEnemiesPerSpawner; i++)
+                {
+                    // get a random index for the enemy prefab array
+                    int randomIndex = Random.Range(0, enemyPrefabs.Length);
+
+                    // spawn a random enemy prefab at the spawner's position
+                    Instantiate(enemyPrefabs[randomIndex], spawner.transform.position, Quaternion.identity);
+
+                    // wait for the specified spawn delay before spawning the next enemy
+                    StartCoroutine(SpawnDelay(spawnDelay));
+                }
+            }
+        }
+
+        // wait until all enemies are killed before starting the next round
+        while (GameObject.FindWithTag("Zombie") != null)
+        {
+            yield return null;
+        }
+
+        // increment the current round
+        currentRound++;
+        DifficultyIncrease();
+
+        // start the next round
+        StartCoroutine(SpawnEnemiesWithDelay());
+    }
+
+    private void DifficultyIncrease()
+    {
+        maxEnemiesPerSpawner = currentRound * maxEnemiesPerSpawner;
+    }
+
+    private IEnumerator SpawnDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+    }
+}
