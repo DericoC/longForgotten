734e01277210c6090f8071ef817446f3e4e51969
monsterwhat <monster_what@hotmail.com>
2023-04-25 18:47:58 -0600

    SUPER UPDATE
    Added Colliders to all objects in the first floor!
    Updated UI to show Score And Time!

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreController : MonoBehaviour
@@ -7,10 +9,30 @@ public class ScoreController : MonoBehaviour
     public int score = 0;
 
     private float gameTimer = 0.0f;
+    public GameObject ScoreComponent;
+    private TextMeshProUGUI scoreText;
+    private string timeString;
 
     private void Update()
     {
-        gameTimer += Time.deltaTime;
+        scoreText = ScoreComponent.GetComponent<TextMeshProUGUI>();
+
+        UpdateTimer();
+
+        updateText();
     }
 
+    void UpdateTimer()
+    {
+        gameTimer += Time.deltaTime;
+        // Format the game time into minutes and seconds
+        TimeSpan time = TimeSpan.FromSeconds(gameTimer);
+        timeString = string.Format("{0:D2}:{1:D2}", time.Minutes, time.Seconds);
+    }
+
+
+    void updateText()
+    {
+        scoreText.text = "Score: " + score + "\nTime: " + timeString;
+    }
 }
